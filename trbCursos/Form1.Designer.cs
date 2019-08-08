a8aa50c61250938dde1cfad8a6643fa8e578b530
kleys <klesyongomes@desbravalink.com>
2019-08-07 21:53:51 -0300

    Commit inicial do projeto, com arquivo .mbd do banco de dados access

---
+++ trbCursos/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace trbCursos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clbCurso = new System.Windows.Forms.CheckedListBox();
+            this.clbDisciplinas = new System.Windows.Forms.CheckedListBox();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.dgvGrade = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGrade)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clbCurso
+            // 
+            this.clbCurso.FormattingEnabled = true;
+            this.clbCurso.Location = new System.Drawing.Point(12, 12);
+            this.clbCurso.Name = "clbCurso";
+            this.clbCurso.Size = new System.Drawing.Size(189, 424);
+            this.clbCurso.TabIndex = 0;
+            this.clbCurso.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.ClbCurso_ItemCheck);
+            // 
+            // clbDisciplinas
+            // 
+            this.clbDisciplinas.FormattingEnabled = true;
+            this.clbDisciplinas.Location = new System.Drawing.Point(224, 12);
+            this.clbDisciplinas.Name = "clbDisciplinas";
+            this.clbDisciplinas.Size = new System.Drawing.Size(189, 424);
+            this.clbDisciplinas.TabIndex = 1;
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(429, 184);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(103, 44);
+            this.btnGerar.TabIndex = 2;
+            this.btnGerar.Text = "GERAR GRADE";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.BtnGerar_Click);
+            // 
+            // dgvGrade
+            // 
+            this.dgvGrade.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGrade.Location = new System.Drawing.Point(548, 12);
+            this.dgvGrade.Name = "dgvGrade";
+            this.dgvGrade.Size = new System.Drawing.Size(240, 424);
+            this.dgvGrade.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvGrade);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.clbDisciplinas);
+            this.Controls.Add(this.clbCurso);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGrade)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.CheckedListBox clbCurso;
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.DataGridView dgvGrade;
+        public System.Windows.Forms.CheckedListBox clbDisciplinas;
+    }
+}
+
---
+++ trbCursos/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace trbCursos
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conexao = new OleDbConnection();
+        OleDbConnection conexaoInsert = new OleDbConnection();
+
+        OleDbCommand comandoSelect = new OleDbCommand();
+        OleDbCommand comandoInsert = new OleDbCommand();
+
+        OleDbDataReader dr;
+        OleDbDataReader dr2;
+
+        int intCodCurso;
+        string strNomeCurso;
+        //string strCodDisciplina;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Conexao com Banco Access 2002-2003
+            conexao.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=C:\Users\Public\dbCurso.mdb; User Id=admin; Password=";
+            comandoSelect.Connection = conexao;
+            conexaoInsert.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=C:\Users\Public\dbCurso.mdb; User Id=admin; Password=";
+            comandoInsert.Connection = conexaoInsert;
+
+            conexao.Open();
+            conexaoInsert.Open();
+            CarregarCursos();
+            CarregarDisciplinas();
+
+            conexao.Close();
+        }
+        
+        private void InserirCodigoC()
+        {
+            clbCurso.Items.Clear();
+            try
+            {
+                string strNoQuery = "insert into tbGrade (txtCodigoC) values ("+ intCodCurso +")";
+                comandoInsert.CommandText = strNoQuery;
+                comandoInsert.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+            }
+        }
+        
+        private void CarregarCursos()
+        {
+            clbCurso.Items.Clear();
+            try
+            {
+                string strQuery = "select txtNomeC from tbCursos";
+                comandoSelect.CommandText = strQuery;
+                dr = comandoSelect.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                       clbCurso.Items.Add(dr[0].ToString());
+                    }
+                }
+                dr.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+            }
+        }
+        private void CarregarDisciplinas()
+        {
+            clbDisciplinas.Items.Clear();
+            try
+            {
+                string strQuery = "select txtNomeD from tbDisciplinas";
+                comandoSelect.CommandText = strQuery;
+                dr = comandoSelect.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        clbDisciplinas.Items.Add(dr[0].ToString());
+                    }
+                }
+                dr.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+            }
+
+
+        }
+
+        private void ClbCurso_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            intCodCurso = Convert.ToInt32(clbCurso.SelectedIndex.ToString());
+            strNomeCurso = clbCurso.Text;
+            
+
+
+        }
+
+        private void BtnGerar_Click(object sender, EventArgs e)
+        {
+            //Quando click é consultado o codigo do curso usando como where o curso marcado 
+            string strQuery = "select txtCodigoC from tbCursos where txtNomeC = '" + strNomeCurso +"'";
+            comandoSelect.CommandText = strQuery;
+            conexao.Open();
+            dr2 = comandoSelect.ExecuteReader();
+
+            if (dr2.HasRows)
+            {
+               while (dr2.Read())
+               {
+                 string textoteste = dr2[0].ToString();
+                 MessageBox.Show(textoteste);
+               }
+            }
+            dr2.Close();
+            conexao.Close(); //fecha conexao para poder ser selecionada outro curso 
+            //Limpar o curso que está marcado perguntar ao mestre 
+            //#
+
+            conexaoInsert.Close();
+        }
+    }
+}
